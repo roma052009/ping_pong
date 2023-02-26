aa3fce3801acb6a6f997fed5e55e5ade2fe16720
roma052009 <124504712+roma052009@users.noreply.github.com>
2023-02-26 11:54:21 +0200

    Add files via upload

--- ping-pong.py
+++ ping-pong.py
@@ -1,6 +1,8 @@
 from pygame import*
 from random import*
 
+font.init()
+
 class GameSprite(sprite.Sprite):
     def __init__(self, player_width, player_height, player_image, player_x, player_y, player_speed):
         self.width = player_width
@@ -27,6 +29,10 @@ class Player(GameSprite):
                 self.rect.y -= self.speed
             if keys[K_DOWN] and self.rect.y < win_height - 20:
                 self.rect.y += self.speed
+            
+class Button(GameSprite):
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
 
 class Ball(GameSprite):
     def set_route(self, ruh_po_x, ruh_po_y):
@@ -36,6 +42,10 @@ class Ball(GameSprite):
         global game
         global game_not_finished
         global text_lose
+        global balls_missed_l
+        global balls_missed_r
+        global text_l
+        global text_r
         if self.rect.y <= 0 or self.rect.y >= win_height - 10:
             self.ruh_po_y = self.ruh_po_y * -1
         
@@ -43,19 +53,43 @@ class Ball(GameSprite):
             self.ruh_po_x = self.ruh_po_x * -1
 
         if self.rect.x <= 0:
-            font.init()
+            
             font1 = font.SysFont('Arial', 50)
-            text_lose = font1.render("Програв синій", 1, (0, 0, 255))
-            game_not_finished = False
+            reset_position()
+            balls_missed_l += 1
+            text_l = font2.render("Пропущено " + str(balls_missed_l), 1, (0, 0, 255))
+            
+
         elif self.rect.x >= win_width:
-            font.init()
+            
             font1 = font.SysFont('Arial', 50)
-            text_lose = font1.render("Програв червоний", 1, (255, 0, 0))
-            game_not_finished = False
+            reset_position()
+            balls_missed_r += 1
+            text_r = font2.render("Пропущено " + str(balls_missed_r), 1, (255, 0, 0))
+            
+            
 
         self.rect.x += self.ruh_po_x
         self.rect.y += self.ruh_po_y
-        
+
+def reset_game():
+    global balls_missed_l
+    global balls_missed_r
+    global text_lose
+    global text_l
+    global text_r
+
+    balls_missed_l = 0
+    balls_missed_r = 0
+    text_lose = font1.render(" ", 1, (255, 0, 0))
+    text_r = font2.render("Пропущено " + str(balls_missed_r), 1, (255, 0, 0))
+    text_l = font2.render("Пропущено " + str(balls_missed_l), 1, (0, 0, 255))
+
+def reset_position():
+    player_l.rect.y = win_height / 2 - 25
+    player_r.rect.y = win_height / 2 - 25
+    ball.rect.x = win_width / 2 - 10
+    ball.rect.y = win_height / 2 - 10      
         
 
         
@@ -65,6 +99,17 @@ FPS = 60
 
 win_width = 700
 win_height = 500
+balls_missed_l = 0
+balls_missed_r = 0
+
+
+
+font1 = font.SysFont('Arial', 50)
+text_lose = font1.render(" ", 1, (255, 0, 0))
+
+font2 = font.SysFont('Arial', 20)
+text_l = font2.render("Пропущено " + str(balls_missed_l), 1, (0, 0, 255))
+text_r = font2.render("Пропущено " + str(balls_missed_r), 1, (255, 0, 0))
 
 window = display.set_mode((win_width, win_height))
 display.set_caption("Пінг понг")
@@ -78,31 +123,63 @@ player_r = Player(10, 100, "r_ping_pong.png", win_width - 13, win_height / 2 - 2
 ball = Ball(20, 20, "ball_ping_pong.png", win_width / 2 - 10, win_height / 2 - 10, 0)
 ball.set_route(randint(3, 5), randint(3, 5))
 
-
+b_play = Button(100, 30, "b_play.jpg", win_width / 2 - 50, win_height / 2 - 45, 0)
+b_restart = Button(100, 30, "b_restart.jpg", win_width / 2 - 50, win_height / 2 - 15, 0)
+b_exit = Button(100, 30, "b_exit.jpg", win_width / 2 - 50, win_height / 2 + 15, 0)
 
 
 game_not_finished = True
 game = True
 while game:
+
+    window.blit(background, (0, 0))
+    player_l.reset()
+    player_r.reset()
+    ball.reset()
+    window.blit(text_l, (0, 0))
+    window.blit(text_r, (win_width - 120, 0))
+
     if game_not_finished == True:
-        window.blit(background, (0, 0))
         player_l.update("l")
         player_r.update("r")
         ball.update()
 
     else:
-        window.blit(background, (0, 0))
-        window.blit(text_lose, (win_width / 4, win_height / 2))
+        window.blit(text_lose, (win_width / 4, 25))
+        b_play.reset()
+        b_restart.reset()
+        b_exit.reset()
+        
+    if balls_missed_l == 5:
+        font1 = font.SysFont('Arial', 50)
+        text_lose = font1.render("Виграв червоний", 1, (255, 0, 0))
+        game_not_finished = False
 
-
+    if balls_missed_r == 5:
+        font1 = font.SysFont('Arial', 50)
+        text_lose = font1.render("Виграв синій", 1, (0, 0, 255))
+        game_not_finished = False
     
-    player_l.reset()
-    player_r.reset()
-    ball.reset()
+    keys = key.get_pressed()
+    if keys[K_ESCAPE]:
+        game_not_finished = False
+
+
     
     display.update()
     clock.tick(FPS)
     
     for e in event.get():
+        if e.type == MOUSEBUTTONDOWN and e.button == 1:
+            x, y = e.pos
+            if b_play.collidepoint(x, y):
+                game_not_finished = True
+            if b_restart.collidepoint(x, y):
+                reset_position()
+                reset_game()
+                game_not_finished = True
+            if b_exit.collidepoint(x, y):
+                game = False
+
         if e.type == QUIT:
             game = False
